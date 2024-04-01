cfb0ad7025465ed53e09b0443955cb1700c5a2c0
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-04-01 12:25:36 -0400

    query API with AsyncHttpClient

--- app/src/main/java/com/example/recipescroll/MainActivity.kt
+++ app/src/main/java/com/example/recipescroll/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.recipescroll
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        webQuery("salad")
+    }
+
+    private fun webQuery(keyword : String) {
+        val client = AsyncHttpClient()
+
+        client["https://www.themealdb.com/api/json/v1/1/search.php?s=$keyword", object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("Recipe Success", "$json")
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Recipe Error", errorResponse)
+            }
+        }]
     }
 }
